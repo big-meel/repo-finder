7e02733382e90b51880a648fb4ed096173cf0d0b
big-meel <big.meel@zoho.com>
2021-09-22 06:22:16 -0500

    Add tests

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "httparty", "~> 0.19.0"
+
+gem "rexml", "~> 3.2"
--- test/models/repo_test.rb
+++ test/models/repo_test.rb
@@ -1,5 +1,10 @@
 require "test_helper"
 
 class RepoTest < ActiveSupport::TestCase
-  
+
+  test "should return list of repositories" do
+    repos = Repo.search_repos("elixir")
+
+    assert repos["items"].any?
+  end
 end
